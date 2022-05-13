fcab1d7ca888cf9862bf2f5af3b7b54db133e051
erdiansyah-me <erdiansyah465@gmail.com>
2022-05-14 04:32:23 +0700

    feat: initiate login activity

---
+++ app/src/main/java/com/safiraak/validin/presentation/view/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.safiraak.validin.presentation.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.safiraak.validin.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
