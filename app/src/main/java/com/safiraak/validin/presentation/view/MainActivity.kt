802f6f495b2d9027da07e050265fd476e1e6af4e
adinekinar <adinekinari@gmail.com>
2022-05-19 15:21:44 +0700

    camera & overlay

--- app/src/main/java/com/safiraak/validin/presentation/view/MainActivity.kt
+++ app/src/main/java/com/safiraak/validin/presentation/view/MainActivity.kt
@@ -1,22 +1,27 @@
 package com.safiraak.validin.presentation.view
 
 import android.content.Intent
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.ActionBarDrawerToggle
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.safiraak.validin.R
 import com.safiraak.validin.databinding.ActivityMainBinding
+import com.safiraak.validin.utils.CamUtils
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var toggle: ActionBarDrawerToggle
     private lateinit var binding: ActivityMainBinding
     private lateinit var auth: FirebaseAuth
+    private var getFile: File? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,7 +39,7 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
         binding.mainAppBar.cardCam.setOnClickListener{
-            startActivity(Intent(this, VerificationActivity1::class.java))
+            cameraXGo()
         }
 
         binding.navView.itemIconTintList = null
@@ -70,6 +75,28 @@ class MainActivity : AppCompatActivity() {
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun cameraXGo() {
+        val intent = Intent(this, CameraxActivity::class.java)
+        cameraLauncher.launch(intent)
+    }
+
+    private val cameraLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        if (it.resultCode == CAMERAX_RESULT) {
+            val file = it.data?.getSerializableExtra("picture") as File
+            val backCam = it.data?.getBooleanExtra("backCam", true) as Boolean
+            val result = CamUtils().rotateBitmap(
+                BitmapFactory.decodeFile(file.path),
+                backCam
+            )
+            val backFile = CamUtils().bitmap2File(this, result)
+            getFile = backFile
+        }
+    }
+
+    companion object {
+        const val CAMERAX_RESULT = 200
     }
 
     private fun showMessage(message: String) {
---
+++ app/src/main/java/com/safiraak/validin/utils/CamUtils.kt
@@ -0,0 +1,118 @@
+package com.safiraak.validin.utils
+
+import android.app.Application
+import android.content.ContentResolver
+import android.content.Context
+import android.content.ContextWrapper
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.net.Uri
+import android.os.Environment
+import android.view.View
+import com.safiraak.validin.R
+import java.io.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+class CamUtils {
+    val FILENAME_FORMAT = "dd-MMM-yyyy"
+
+    val timeStamp: String = SimpleDateFormat(
+        FILENAME_FORMAT,
+        Locale.US
+    ).format(System.currentTimeMillis())
+
+    fun cropImage(bitmap: Bitmap, frame: View, reference: View): ByteArray {
+        val heightOriginal = frame.height
+        val widthOriginal = frame.width
+        val heightFrame = reference.height
+        val widthFrame = reference.width
+        val leftFrame = reference.left
+        val topFrame = reference.top
+        val heightReal = bitmap.height
+        val widthReal = bitmap.width
+        val widthFinal = widthFrame * widthReal / widthOriginal
+        val heightFinal = heightFrame * heightReal / heightOriginal
+        val leftFinal = leftFrame * widthReal / widthOriginal
+        val topFinal = topFrame * heightReal / heightOriginal
+        val bitmapFinal = Bitmap.createBitmap(
+            bitmap,
+            leftFinal, topFinal, widthFinal, heightFinal
+        )
+        val stream = ByteArrayOutputStream()
+        bitmapFinal.compress(
+            Bitmap.CompressFormat.JPEG,
+            100,
+            stream
+        )
+        return stream.toByteArray()
+    }
+
+    fun makeFile(application: Application): File {
+        val dir = application.externalMediaDirs.firstOrNull()?.let {
+            File(it, application.resources.getString(R.string.app_name)).apply { mkdirs() }
+        }
+
+        val outDir = if (dir != null && dir.exists()) dir else application.filesDir
+
+        return File(outDir, "$timeStamp.jpg")
+    }
+
+    fun reduceSize(file: File): File {
+        val bitmap = BitmapFactory.decodeFile(file.path)
+        var theQuality = 100
+        var lengthStream: Int
+
+        do {
+            val streamBitmap = ByteArrayOutputStream()
+            bitmap.compress(Bitmap.CompressFormat.JPEG,theQuality, streamBitmap)
+            val pictByteArray = streamBitmap.toByteArray()
+            lengthStream = pictByteArray.size
+            theQuality -= 5
+        } while (lengthStream > 1000000)
+        bitmap.compress(Bitmap.CompressFormat.JPEG, theQuality, FileOutputStream(file))
+        return file
+    }
+
+    fun bitmap2File(context: Context, bitmap: Bitmap) : File {
+        val wrapper = ContextWrapper(context)
+        var file = wrapper.getDir("Images", Context.MODE_PRIVATE)
+        file = File(file, "${UUID.randomUUID()}.jpg")
+        val streamOut: OutputStream = FileOutputStream(file)
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 25, streamOut)
+        streamOut.flush()
+        streamOut.close()
+        return file
+    }
+
+    fun uri2File(selectImage: Uri, context: Context): File {
+        val contentResolve: ContentResolver = context.contentResolver
+        val file = createTempFile(context)
+        val inputStream = contentResolve.openInputStream(selectImage) as InputStream
+        val outputStream: OutputStream = FileOutputStream(file)
+        val buf = ByteArray(1024)
+        var len: Int
+        while (inputStream.read(buf).also { len = it } > 0) outputStream.write(buf, 0, len)
+        outputStream.close()
+        inputStream.close()
+        return file
+    }
+
+    fun createTempFile(context: Context): File {
+        val dir: File? = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(timeStamp, ".jpg", dir)
+    }
+
+    fun rotateBitmap(bitmap: Bitmap, backCam: Boolean = false): Bitmap {
+        val matrix = Matrix()
+        return if (backCam) {
+            matrix.postRotate(90f)
+            Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
+        } else {
+            matrix.postRotate(-90f)
+            matrix.postScale(-1f, 1f, bitmap.width/2f, bitmap.height/2f)
+            Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
+        }
+    }
+}
