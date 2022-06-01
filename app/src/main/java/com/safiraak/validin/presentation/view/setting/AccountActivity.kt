bf5e0e15a6b34f923f2ddbb3fb95fe3452e969a5
erdiansyah-me <erdiansyah465@gmail.com>
2022-06-01 23:24:46 +0700

    Fix :  Merge firebase-auth with main

---
+++ app/src/main/java/com/safiraak/validin/presentation/view/setting/AccountActivity.kt
@@ -0,0 +1,69 @@
+package com.safiraak.validin.presentation.view.setting
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.activity.viewModels
+import com.bumptech.glide.Glide
+import com.safiraak.validin.R
+import com.safiraak.validin.databinding.ActivityAccountBinding
+import com.safiraak.validin.presentation.view.auth.LoginActivity
+import com.safiraak.validin.presentation.view.main.PopUpUsernameFragment
+import com.safiraak.validin.presentation.viewmodel.UserViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class AccountActivity : AppCompatActivity(), PopUpUsernameFragment.PopUpUsernameListener{
+
+    private lateinit var binding: ActivityAccountBinding
+    private val userViewModel: UserViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        userViewModel.user.observe(this) {
+            if (it == null) {
+                startActivity(Intent(this, LoginActivity::class.java))
+                finish()
+            } else {
+                binding.apply {
+                    uname.text = it.displayName
+                    email.text = it.email
+                    Glide.with(applicationContext)
+                        .load(it.photoUrl)
+                        .centerCrop()
+                        .error(R.drawable.pierre)
+                        .into(imgProfile)
+                    buttonEditName.setOnClickListener {
+                        val popUpUsernameFragment = PopUpUsernameFragment(getString(R.string.edit_profile_name))
+                        popUpUsernameFragment.show(supportFragmentManager,"Pop Up Username")
+                    }
+                    buttonPass.setOnClickListener {
+                        //EDIT PASSWORD
+                    }
+                }
+            }
+        }
+
+        binding = ActivityAccountBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.accToolbar)
+
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home){
+            onBackPressed()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun getUsername(username: String) {
+        userViewModel.editUserName(username)
+        binding.uname.text = username
+    }
+}
---
+++ app/src/main/java/com/safiraak/validin/presentation/view/verify/VerificationActivity2.kt
@@ -0,0 +1,16 @@
+package com.safiraak.validin.presentation.view.verify
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.safiraak.validin.databinding.ActivityVerification2Binding
+
+class VerificationActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivityVerification2Binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityVerification2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+    }
+}
