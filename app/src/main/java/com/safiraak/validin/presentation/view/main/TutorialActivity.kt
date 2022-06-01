21e97ba5b6d6ed0541193f56c0624e98a59c3762
adinekinar <adinekinari@gmail.com>
2022-06-01 16:30:33 +0700

    finish motion layout

--- app/src/main/java/com/safiraak/validin/presentation/view/main/TutorialActivity.kt
+++ app/src/main/java/com/safiraak/validin/presentation/view/main/TutorialActivity.kt
@@ -1,5 +1,6 @@
 package com.safiraak.validin.presentation.view.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.safiraak.validin.databinding.ActivityMainBinding
@@ -13,7 +14,9 @@ class TutorialActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityTutorialBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        
+
         supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        binding.buttonBackHome.setOnClickListener { startActivity(Intent(this, MainActivity::class.java)) }
     }
 }
